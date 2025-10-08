6970b67ce6be2c09b4fcf15dbc8a9eff22fe9e5f
Brodye Stevens <aubiestevens03@gmail.com>
2025-10-07 23:43:31 -0500

    Initial Project Setup

---
+++ Fall2025-Project2-bdstevens2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2025_Project2_bdstevens2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
